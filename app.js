93338664088bed7eec3cec82a0b3992b09627c9e
Avinash10098 <avinashfulzele5@gmail.com>
2024-03-11 20:14:22 +0530

    update

--- app.js
+++ app.js
@@ -1,56 +1,44 @@
 function locomotiveAnimation() {
 
-
     gsap.registerPlugin(ScrollTrigger);
 
-    // new code 
-    const scroll = new LocomotiveScroll({
-        el: document.querySelector('#main'),
-        smooth: true,
-        lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
-        multiplier: 1.4, // Effect Multiplier
-        reloadOnContextChange: true,
-        touchMultiplier: 2,
-        smoothMobile: 0,
-        smartphone: {
-            smooth: !0,
-            breakpoint: 767
-        },
-        tablet: {
-            smooth: !1,
-            breakpoint: 1024
-        },
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
     });
-    //new code end
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-    scroll.on("scroll", ScrollTrigger.update);
-
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
     ScrollTrigger.scrollerProxy("#main", {
         scrollTop(value) {
-            return arguments.length
-                ? scroll.scrollTo(value, 0, 0)
-                : scroll.scroll.instance.scroll.y;
-        },
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
         getBoundingClientRect() {
-            return {
-                top: 0,
-                left: 0,
-                width: window.innerWidth,
-                height: window.innerHeight
-            };
-        }
-
-        // follwoing line is not required to work pinning on touch screen
-
-        /* pinType: document.querySelector(".smooth-scroll").style.transform
-          ? "transform"
-          : "fixed"*/
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
     });
 
-    ScrollTrigger.addEventListener("refresh", () => scroll.update());
 
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
 
+    setTimeout(() => {
+        locoScroll.destroy();
+    }, 0);
+    setTimeout(() => {
+        locoScroll.init();
+    }, 50);
+    setTimeout(() => {
+        locoScroll.update();
+    }, 1000);
 }
 locomotiveAnimation();
 
